42f76ae4990078e84714eb0efe58b59010952a61
Chris Mottram <C.Mottram@ljmu.ac.uk>
2017-11-13 16:15:45 +0000

    Added initial version of XMoptopInstrumentConfig.java

---
+++ java/ngat/phase2/XMoptopInstrumentConfig.java
@@ -0,0 +1,37 @@
+package ngat.phase2;
+
+import java.io.Serializable;
+
+public class XMoptopInstrumentConfig extends XInstrumentConfig implements Serializable {
+
+ 	private static final long serialVersionUID = -6080756087703394705L;
+    public final static int DICHROIC_STATE_OUT_BEAM = 1;
+    public final static int DICHROIC_STATE_IN_BEAM = 2;
+
+ 	private int dichroicState;
+    
+	
+	public int getDichroicState() {
+		return dichroicState;
+	}
+
+	public void setDichroicState(int dichroicState) {
+		this.dichroicState = dichroicState;
+	}
+
+	public XMoptopInstrumentConfig() {
+		super();
+	}
+	
+	public XMoptopInstrumentConfig(String name) {
+		super(name);
+	}
+	
+	public String toString() {
+		return this.getClass().getName() +" ["+getID()+"] "+getName()+
+		", instrument="+getInstrumentName()+
+		", detector="+getDetectorConfig()+
+		", dichroicState="+getDichroicState();
+	}
+	
+}
